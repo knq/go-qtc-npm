70308db09cd9cb2e6ea28664dd6c994b31bf5100
Kenneth Shaw <kenneth.shaw@knq.io>
2017-07-06 11:54:20 +0700

    Fixing issues with windows

--- build.sh
+++ build.sh
@@ -7,5 +7,9 @@ PACKAGE=github.com/valyala/quicktemplate/qtc
 go get -u $PACKAGE
 
 for PLATFORM in linux darwin windows; do
-  GOOS=$PLATFORM GOARCH=amd64 go build -o vendor/go-qtc-$PLATFORM $PACKAGE
+  EXT=""
+  if [ "$PLATFORM" = "windows" ]; then
+    EXT=".exe"
+  fi
+  GOOS=$PLATFORM GOARCH=amd64 go build -o vendor/go-qtc-$PLATFORM$EXT $PACKAGE
 done
